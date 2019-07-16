f4e450df03c9e4127e04181cf8e012d2433b3ae6
nickhyde <nickhyde314@yahoo.com>
2019-07-15 18:03:44 -0700

    remove pc_database and config from cache

--- config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-database_location = '/home/marc/Desktop/podcatcher/pc_database.db'
-
-audio_default_location = '/home/marc/Desktop'
-video_default_location = '/home/marc/Desktop'
-
